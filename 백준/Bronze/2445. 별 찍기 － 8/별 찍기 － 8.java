23b3aa20179c54056fc80a355a3218d8c28c44eb
serethia <137035446+serethia@users.noreply.github.com>
2023-07-27 23:55:57 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 648 ms, Memory: 18220 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = n - 1; i >= 1; i--) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("*");
+			}
+			for (int j = 1; j <= 2 * i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		for (int i = 1; i <= 2 * n; i++) {
+			System.out.print("*");
+		}
+		System.out.println(" ");
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("*");
+			}
+			for (int j = 1; j <= 2 * i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		sc.close();
+	}
+}
