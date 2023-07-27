89a6baf06318fbfd54a7950977ab269576d28225
serethia <137035446+serethia@users.noreply.github.com>
2023-07-27 23:53:22 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 196 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = n; i >= 1; i--) {
+			for (int j = n; j >= n+1-i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
