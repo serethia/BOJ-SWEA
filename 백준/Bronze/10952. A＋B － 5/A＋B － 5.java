d8d72f984bb7848ce005d2fe6acf7bbd2cd2d3cd
serethia <137035446+serethia@users.noreply.github.com>
2023-07-28 20:25:20 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 13072 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			if (a + b == 0)
+				break;
+			System.out.println(a + b);
+		}
+		sc.close();
+	}
+}
