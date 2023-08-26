37b81f0e26a67e23ca626513a661ba827911f426
serethia <137035446+serethia@users.noreply.github.com>
2023-08-26 19:53:48 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기1, Time: 127 ms, Memory: 20,240 KB -BaekjoonHub

---
+++ SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.Stack;
+ 
+public class Solution {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        for (int tc = 1; tc <= 10; tc++) { // 테케
+            int n = sc.nextInt(); // 문자열 계산식 길이
+            String str = sc.next();
+            Stack<Character> charstack = new Stack<>(); // 연산자 스택
+            Stack<Integer> intstack = new Stack<>(); // 피연산자 스택
+            char[] arr = new char[n]; // 원본 배열
+            char[] sortarr = new char[n]; // 후위 배열
+            for (int i = 0; i < n; i++) {
+                arr[i] = str.charAt(i);
+            }
+ 
+            // 후위 전환 (연산자 스택)
+            int idx = 0;
+            for (int i = 0; i < n; i++) {
+                if (arr[i] == '+') {
+                    while (!charstack.isEmpty()) { // 우선순위 고려 && stack.peek() >= arr[i]
+                        sortarr[idx++] = charstack.pop();
+                    }
+                    charstack.push(arr[i]);
+                } else {
+                    sortarr[idx++] = arr[i];
+                }
+            }
+ 
+            for (int j = 0; j < n; j++) {
+                while (!charstack.isEmpty() && sortarr[j] == 0) {
+                    sortarr[j] = charstack.pop();
+                }
+            }
+ 
+            // 후위 계산 (피연산자 스택)
+            for (int i = 0; i < n; i++) {
+                if (sortarr[i] != '+') {
+                    intstack.push((sortarr[i]) - '0');
+                }
+                if (!intstack.isEmpty() && sortarr[i] == '+') {
+                    int b = intstack.pop();
+                    int a = intstack.pop();
+                    intstack.push(a + b);
+                }
+            }
+            System.out.println("#" + tc + " " + intstack.pop());
+        }
+    }
+}
