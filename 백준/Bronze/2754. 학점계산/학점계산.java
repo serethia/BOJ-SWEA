6c22e45a251cb829ded59afbfc4a32dacf5c6eb8
serethia <137035446+serethia@users.noreply.github.com>
2023-09-03 20:26:13 +0900

    [Bronze V] Title: 학점계산, Time: 112 ms, Memory: 12904 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Map<String, Double> map = new HashMap<>();
+		map.put("A+", 4.3);
+		map.put("A0", 4.0);
+		map.put("A-", 3.7);
+		map.put("B+", 3.3);
+		map.put("B0", 3.0);
+		map.put("B-", 2.7);
+		map.put("C+", 2.3);
+		map.put("C0", 2.0);
+		map.put("C-", 1.7);
+		map.put("D+", 1.3);
+		map.put("D0", 1.0);
+		map.put("D-", 0.7);
+		map.put("F", 0.0);
+		System.out.println(map.get(sc.next()));
+	}
+}
