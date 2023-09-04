e7a1e74e9f492aab515a151f7921fe885f2d13de
serethia <137035446+serethia@users.noreply.github.com>
2023-09-04 20:02:59 +0900

    [Bronze III] Title: 알람 시계, Time: 112 ms, Memory: 12892 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		if (m < 45) {
+			h--;
+			m += 15;
+			if (h < 0) {
+				h += 24;
+			}
+		} else {
+			m -= 45;
+		}
+		System.out.println(h + " " + m);
+	}
+}
