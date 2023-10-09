f99ffc9cd10ec17a70d2f806106bf010a8c1c0ff
serethia <137035446+serethia@users.noreply.github.com>
2023-10-09 15:31:20 +0900

    [Silver II] Title: N과 M (9), Time: 120 ms, Memory: 16136 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+	static int m;
+	static int[] nums;
+	static int[] select;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder(); // 더 빠른 출력을 위함
+
+	public static void main(String[] args) throws NumberFormatException, IOException { // 스캐너: 시간 초과 => 버퍼 사용
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()); // 공백 단위로 입력 받기
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		select = new int[m]; // 뽑은 숫자 저장
+		nums = new int[n]; // 입력값 저장
+		visited = new boolean[n]; // 방문 처리
+		st = new StringTokenizer(br.readLine()); // 줄바꿈
+		for (int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(nums);
+		perm(0); // sidx: select의 시작 인덱스
+		System.out.println(sb);
+	}
+
+	public static void perm(int sidx) throws IOException {
+		if (sidx == m) { // m개 뽑으면 종료
+			for (int each : select) {
+				sb.append(each).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		int same = 0;
+		for (int i = 0; i < n; i++) {
+			if (!visited[i] && same != nums[i]) { // 미방문 + 중복 아닐 시
+				visited[i] = true;
+				select[sidx] = nums[i];
+				same = nums[i]; // 이전 결과와 중복 안 되게
+				perm(sidx + 1);
+				visited[i] = false;
+			}
+		}
+	}
+}
