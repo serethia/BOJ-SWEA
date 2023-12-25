7c788d91d0c7e0f433cb5b6d47301473df8797c2
Sewon Oum <137035446+serethia@users.noreply.github.com>
2023-12-25 11:16:11 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 648 ms, Memory: 57516 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static class Dot implements Comparable<Dot> {
+		int x;
+		int y;
+
+		public Dot(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int compareTo(Dot o) {
+			// 1. y좌표 비교
+			int yresult = Integer.compare(this.y, o.y);
+			if (yresult == 0) {
+				// 2. x좌표 비교
+				int xresult = Integer.compare(this.x, o.x);
+				return xresult;
+			} else {
+				return yresult;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 시간 단축용
+
+		// 저장
+		int n = Integer.parseInt(br.readLine());
+		List<Dot> list = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			list.add(new Dot(x, y));
+		}
+
+		// 정렬
+		Collections.sort(list);
+
+		// 출력
+		StringBuilder sb = new StringBuilder();
+		for (Dot d : list) {
+			sb.append(d.x).append(" ").append(d.y).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
