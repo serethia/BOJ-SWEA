e6fcef69e4ff97e7cf83c49464b366243b9102b0
Sewon Oum <137035446+serethia@users.noreply.github.com>
2024-01-28 11:39:36 +0900

    [Bronze I] Title: 세로읽기, Time: 76 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[][] map = new char[5][15];
+		int[] rowlen = new int[5];
+		sb = new StringBuilder();
+		for (int r = 0; r < 5; r++) {
+			String row = br.readLine();
+			for (int c = 0; c < row.length(); c++) {
+				map[r][c] = row.charAt(c);
+			}
+		}
+		for (int c = 0; c < 15; c++) {
+			for (int r = 0; r < 5; r++) {
+				if (map[r][c] == '\0') { // 값이 없을 때 (char에서의 null)
+					continue;
+				} else {
+					sb.append(map[r][c]);
+				}
+			}
+		}
+		System.out.println(sb);
+	}
+}
